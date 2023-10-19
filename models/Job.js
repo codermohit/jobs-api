f625c0b2dc6969fabd45688b0fc2405825d1705c
CoderMohit <iamthecodermohit@gmail.com>
2023-10-20 02:10:57 +0530

    first commit

---
+++ models/Job.js
@@ -0,0 +1,30 @@
+const { string } = require("joi");
+const mongoose = require("mongoose");
+
+const JobsSchema = mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Job", JobsSchema);
